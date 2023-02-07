a6f3c52c2bb5f6e94bc40dcd8e2c04d4bbcaf747
DESKTOP-CGH48O5\laure <laurenkim002@naver.com>
2023-02-07 17:11:50 +0900

    [김혜민] - post 실습 2문제

---
+++ src/main/java/com/malloc/servlet/servlet/test/Test09Controller.java
@@ -0,0 +1,32 @@
+package com.malloc.servlet.servlet.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/test09")
+public class Test09Controller extends HttpServlet {
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("name");
+		String text = request.getParameter("text");
+		
+		out.println("<html><head><title>지원 완료</title></head> <body>");
+		
+		out.println("<h2>" + name + "님 지원이 완료 되었습니다.</h2><hr>");
+		out.println("지원 내용<br>");
+		out.println(text);
+		
+		out.println("</body></html>");
+		
+	}
+}
