b118614a1b374ac198d7a9ac008845db1abfe661
DESKTOP-CGH48O5\laure <laurenkim002@naver.com>
2023-02-21 17:34:40 +0900

    [김혜민] - db 실습 1문제 test01

--- src/main/java/com/malloc/servlet/database/Test01Controller.java
+++ src/main/java/com/malloc/servlet/database/Test01Controller.java
@@ -1,5 +1,10 @@
 package com.malloc.servlet.database;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,18 +16,50 @@ import com.malloc.servlet.common.MysqlService;
 public class Test01Controller extends HttpServlet {
 	
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.setContentType("text/plain");
+		
+		PrintWriter out = response.getWriter();
 		
 		MysqlService mysqlService = MysqlService.getInstance();
 		
-		String query = "INSERT INTO `real_estate`\r\n"
-				+ "(`realtorId`, `address`, `area`, `type`, `price`, `rentPrice`, `createdAt`, `updatedAt`)\r\n"
-				+ "value\r\n"
-				+ "(3, '헤라펠리스 101동 5305호', 350, '매매', 1500000, NULL, now(), now());";
+		//접속
+		mysqlService.connect();
 		
-		int count = mysqlService.update(query);
+		//쿼리 작성
+//		String insertQuery = "INSERT INTO `real_estate`\r\n"
+//				+ "(`realtorId`, `address`, `area`, `type`, `price`, `rentPrice`, `createdAt`, `updatedAt`)\r\n"
+//				+ "value\r\n"
+//				+ "(3, '헤라펠리스 101동 5305호', 350, '매매', 1500000, NULL, now(), now());";
+//		
+//		int count = mysqlService.update(insertQuery);
+//		
+//		out.println(count);
 		
+		String selectQuery = "SELECT `address`, `area`, `type`\r\n"
+				+ "FROM `real_estate`\r\n"
+				+ "ORDER BY id DESC\r\n"
+				+ "LIMIT 10;";
+		
+		ResultSet resultSet = mysqlService.select(selectQuery);
+		
+		try {
+			while(resultSet.next()) {
+				String address = resultSet.getString("address");
+				int area = resultSet.getInt("area");
+				String type = resultSet.getString("type");
+				
+				
+				out.println("매물 주소: " + address + ", 면적: " + area + ", 타입: " + type);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		mysqlService.disconnect();
 		
 	}
 }
